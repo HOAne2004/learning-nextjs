c36c1be91b0c4393640657eafbb7add87419d53d
HOAn04 <lehuyhoan04@gmail.com>
2026-09-25 23:39:04 +0700

    learn: using css module in page

--- practice/learning-nextjs/app/page.tsx
+++ practice/learning-nextjs/app/page.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import style1 from '@/styles/app.module.css'
+import style2 from '@/styles/style2.module.css'
 
 export default function Home() {
   return (
@@ -6,9 +8,12 @@ export default function Home() {
       <h1 className="text-6xl font-bold">
         Home Page
       </h1>
+      <div className={style1['green']}>
+        <span className={style2['green']}>Nội dung trong Homepage</span>
+      </div>
       <a href="admin">Chuyển tới trang Admin (thẻ a)</a>
       <br></br>
-      <Link href={"/admin"}>Chuyển tới trang Admin (thẻ Link)</Link>
+      <Link href={"/admin"} className="green">Chuyển tới trang Admin (thẻ Link)</Link>
     </div>
   );
 }
